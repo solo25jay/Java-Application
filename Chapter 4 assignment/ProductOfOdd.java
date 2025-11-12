6694f9d5424f5c5749ca7cfe4bda493b30dd1aca
solomon obasogie <125708821+solo25jay@users.noreply.github.com>
2025-11-12 10:34:31 +0100

    Delete Chapter 4 assignment directory

--- Chapter 4 assignment/ProductOfOdd.java
+++
@@ -1,8 +0,0 @@
-public class ProductOfOdd {
-    public static void main(String[] args) {
-        long product = 1;
-        for (int i = 1; i <= 15; i += 2)
-            product *= i;
-        System.out.println("Product = " + product);
-    }
-}
